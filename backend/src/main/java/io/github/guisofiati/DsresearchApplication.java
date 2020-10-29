83077b8ed1b40ea14c55aef4447bc0c09fd2f85a
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2020-10-29 09:43:17 -0300

    Project created

---
+++ backend/src/main/java/io/github/guisofiati/DsresearchApplication.java
@@ -0,0 +1,13 @@
+package io.github.guisofiati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsresearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsresearchApplication.class, args);
+	}
+
+}
